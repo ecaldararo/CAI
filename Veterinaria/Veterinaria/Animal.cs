d79ff5ab4a0e1517a694673eb6f37531b2de5370
ecaldararo <ecaldararo@gmail.com>
2021-04-13 22:32:25 -0300

    Segundo Ejercicio - Veterinaria

---
+++ Veterinaria/Veterinaria/Animal.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Veterinaria
+{
+    class Animal
+    {
+        private string _nombre;
+        private int _peso;
+        private int _cantvacunas;
+        private int _codigodueño;
+
+        public Animal(string nombre, int codigodueño)
+        {
+            _nombre = nombre;
+            _codigodueño = codigodueño;
+        }
+
+        public int AsignarPeso { set { _peso = value; } get { return _peso; } }
+        public int Vacunas { set { _cantvacunas = value; } get { return _cantvacunas; } }
+
+        public string DarVacuna()
+        {
+            try
+            {
+                this.Vacunas++;
+                return "Se le dio una vacuna adicional a " + this;
+            }
+            catch (Exception)
+            {
+                return "Falló la asignación de vacunas a " + this;
+            }
+        }
+
+
+
+    }
+}
---
+++ Veterinaria/Veterinaria/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Veterinaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // En soluciones distintas, modelar un problema / negocio general(clase, propiedades, encapsulamiento, abstracción, que se pueda instanciar,
+            // program con set y get) con los siguientes puntos(de ser posible, hacer las cinco soluciones).
+            //- Veterinaria
+            //- Restaurant
+            //- Carta
+            //- Deportista
+            //- Almacén(tratar de no usar producto, que es el ejemplo visto en clase)
+
+            Animal Rosa = new Animal("Rosa", 1);
+            Rosa.AsignarPeso = 16;
+            Rosa.Vacunas = 0;
+            Rosa.DarVacuna();
+
+
+
+        }
+    }
+}
